268c76e63ccd15c3bf25a1c0a0a3d8f27fab7628
Wesley DuSell <dusellfam@gmail.com>
2025-01-01 15:32:46 -0500

    Yay Signals

--- apps/ai-rack-builder/src/app/routes/builds/build/build-components-list-card/build-components-list-card.component.ts
+++ apps/ai-rack-builder/src/app/routes/builds/build/build-components-list-card/build-components-list-card.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-build-components-list-card',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   template: `
     <mat-card>
+      <mat-card-actions align="end">
+        <button mat-button>New Component</button>
+      </mat-card-actions>
       <mat-card-content>
         <p>Components List</p>
       </mat-card-content>
